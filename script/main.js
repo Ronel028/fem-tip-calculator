7adda5217d8f5a128cab9db1bc243ac2d3d8d925
Ronel <shujihanma028@gmail.com>
2021-12-30 15:55:50 -0800

    tip calculator

---
+++ script/main.js
@@ -0,0 +1,104 @@
+let billInput = document.querySelector('#bill-input');
+let tipButton = document.querySelectorAll('.select-tip .select-tip-button button');
+let customTip = document.querySelector('#input-tip');
+let numberPeople = document.querySelector('#number-people');
+let resetButton = document.querySelector('.right-container .reset-btn');
+
+// output
+let tAmount = document.getElementById('tipAmount');
+let total = document.getElementById('totalAmount');
+
+
+let bill;
+let percentage;
+let inputCustomTip;
+let numberPeopleInput = 1;
+
+billInput.addEventListener('input', billValue);
+customTip.addEventListener('input', customTipInput);
+numberPeople.addEventListener('input', peopleInput);
+resetButton.addEventListener('click', reset);
+
+// function for bill value
+function billValue(){
+     bill = parseFloat(billInput.value);
+
+     if(bill < 0){
+          billInput.value = ' ';
+          bill = 0;
+     }
+
+     calculateTip();
+}
+
+// function for custom tip
+function customTipInput(){
+     tipButton.forEach(button => button.classList.remove('active-state-button'));
+     inputCustomTip = parseInt(customTip.value)/100;
+     percentage = inputCustomTip;
+
+     calculateTip();
+}
+
+// function for people input
+function peopleInput(){
+     numberPeopleInput = parseInt(numberPeople.value);
+      
+     calculateTip();
+
+     let errorMessage = document.querySelector('.error-msg');
+     if(numberPeopleInput === 0 || numberPeopleInput < 0){
+          errorMessage.style.display = 'block';
+          tAmount.innerText = "0.00";
+          total.innerText = "0.00";
+          numberPeople.value = 0;
+          numberPeopleInput = 1;
+     }
+     else{
+          errorMessage.style.display = 'none';
+     }
+}
+
+// tip button
+tipButton.forEach(function(btn){
+     btn.addEventListener('click', function(e){
+
+          // Add active state in button
+          tipButton.forEach(button => button.classList.remove('active-state-button'));
+          this.classList.add('active-state-button');
+          customTip.value = '';
+
+
+          percentage = parseInt(e.target.innerText)/100; 
+          calculateTip();       
+     })
+});
+
+// Calculate tip
+
+function calculateTip(){
+
+     let tipAmount = ((bill * percentage)/numberPeopleInput).toFixed(2);
+     tAmount.textContent = tipAmount;
+
+     let totalAmount = ((bill + parseInt(tipAmount))/numberPeopleInput).toFixed(2);
+     total.textContent = totalAmount;
+}
+
+// reset
+ function reset(){
+      billInput.value = '';
+      bill = 0;
+
+      tipButton.forEach(button => button.classList.remove('active-state-button'));
+      customTip.value = '';
+      inputCustomTip = 0;
+      percentage = 0;
+
+      numberPeople.value = 1;
+      numberPeopleInput = 1;
+
+      tAmount.textContent = '0.00';
+      total.textContent = '0.00';
+ }
+
